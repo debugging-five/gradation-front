3dfffbe4e80f8c4bb504bc07c3e7876e35580cb2
alsdk1676 <alsdk6761@gmail.com>
2025-04-23 16:47:02 +0900

    feat: button 컴포넌트 제작 중

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import router from './routes/router';
 import MainContainer from './pages/main/MainContainer';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import TestContainer from './pages/test/TestContainer';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <RouterProvider router={router} />
+      {/* <GlobalStyle /> */}
+      {/* <RouterProvider router={router} /> */}
+      <TestContainer />
     </>
   );
 }
--- src/pages/test/TestContainer.js
+++ src/pages/test/TestContainer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import S from './style';
+import BasicButton2 from '../../components/button/BasicButton2';
+import BasicButton from '../../components/button/BasicButton';
 
 const TestContainer = () => {
   return (
     <div>
-      
+      <BasicButton2>
+        버튼
+      </BasicButton2>
+{/* 
+      <BasicButton>
+        버튼2
+      </BasicButton> */}
     </div>
   );
 };
