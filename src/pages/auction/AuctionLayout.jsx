7269008fd5ca8e1f236b2d09f6e6ed9744160e71
sehyunf <ssam010925@gmail.com>
2025-05-18 21:42:10 +0900

    feat: auctionList type스타일 제작

--- src/pages/auction/AuctionLayout.jsx
+++ src/pages/auction/AuctionLayout.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
-import { Link, Navigate, Outlet, useParams, useSearchParams } from 'react-router-dom';
+import { Link, Navigate, Outlet, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import S from './style';
 
 const AuctionLayout = () => {
 
   const params = useParams()
+  const navigate = useNavigate();
   if(!(params.type === "bidding" || params.type === "expected" || params.type === "complete" || params.type === null)) {
     return <Navigate to="/auction/bidding/korean"></Navigate>
   }
+
+  const goBidding = () => {
+    navigate(`/auction/bidding/korean`);
+  }
+  const goExpected = () => {
+    navigate(`/auction/expected/korean`);
+  }
+  const goComplete = () => {
+    navigate(`/auction/complete/korean`);
+  }
+
   
   
   return (
     <div>
       <S.TypeWrapper>
-        <Link to={"/auction/bidding/korean"}>경매중</Link>
-         |
-        <Link to={"/auction/expected/korean"}>경매 예정</Link>
-         |
-        <Link to={"/auction/complete/korean"}>경매 완료</Link> 
+        <S.TypeDiv>
+          {params.type === "bidding"? 
+            <S.SelectedRed>경매중</S.SelectedRed> : 
+            <S.UnSelectedType onClick={goBidding} >경매중</S.UnSelectedType>
+          }
+        </S.TypeDiv>
+        <S.Bar>|</S.Bar>
+        <S.TypeDiv>
+          {params.type === "expected"? 
+            <S.SelectedRed>경매예정</S.SelectedRed> : 
+            <S.UnSelectedType onClick={goExpected} >경매예정</S.UnSelectedType>
+          }
+        </S.TypeDiv>
+        <S.Bar>|</S.Bar>
+        <S.TypeDiv>
+          {params.type === "complete"? 
+            <S.SelectedRed>경매결과</S.SelectedRed> : 
+            <S.UnSelectedType onClick={goComplete} >경매결과</S.UnSelectedType>
+          }
+        </S.TypeDiv> 
       </S.TypeWrapper>
       <div>
         <Outlet />
--- src/pages/auction/style.js
+++ src/pages/auction/style.js
@@ -1,14 +1,36 @@
 import styled from 'styled-components'
-import { EN_H1, EN_H2 } from '../../styles/common';
+import { EN_H1, EN_H2, H8 } from '../../styles/common';
 const primary = "#EE3333";
 const gray100 = "#FBFCFC";
 const gray500 = "#C0C5C7";
 const gray900 = "#6E7476";
+const black = "#333333";
 const warning = "#E49804";
 
 const S = {};
 S.EN_H2 = styled.p`
   ${EN_H2}
+`
+
+S.H8 = styled.p`
+  ${H8}
+  color: ${black};
+`
+S.UnSelectedType = styled.p`
+  ${H8}
+  color: ${gray900};
+  cursor: pointer;
+  
+  &:hover {
+    color: ${black};
+  }
+`
+
+S.SelectedRed = styled.p`
+  ${H8}
+  color: ${primary};
+  cursor: pointer;
+  border-bottom: solid 2px ${primary};
 `
 
 S.Wrapper = styled.div`
@@ -19,8 +41,22 @@ S.Wrapper = styled.div`
 `
 
 S.TypeWrapper = styled.div`
+  margin: 24px 0;
   display: flex;
 `
+
+S.TypeDiv = styled.div`
+  width: 120px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+S.Bar = styled.p`
+  ${H8}
+`
+
+
 
 
 export default S;
