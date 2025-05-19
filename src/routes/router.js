c89b4327e72109f6bd7da882b2ac194de9cb74e9
Dgeon1008 <Dcode0971@gmail.com>
2025-05-19 17:10:31 +0900

    feat: main 페이지 작업중

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,5 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Layout from "../pages/layout/Layout";
-import Main from "../pages/main/main";
 import ArtistDetailContainer from "../pages/artist/artistDetail/ArtistDetailContainer";
 import ArtistDetail from "../pages/artist/artistDetail/ArtistDetail";
 import LoginLayout from "../pages/layout/LoginLayout";
@@ -96,6 +95,7 @@ import ContactArtistReceivedDetail from "../pages/mypage/myActive/contactArtist/
 import ContactArtistSendedDetail from "../pages/mypage/myActive/contactArtist/ContactArtistSendedDetail";
 import UpcyclingMain from "../pages/upcycling/upcyclingMain/UpcyclingMain";
 import NewPassword from "../pages/account/newPassword/NewPassword";
+import Main from "../pages/main/Main";
 
 const router = createBrowserRouter([
   {
