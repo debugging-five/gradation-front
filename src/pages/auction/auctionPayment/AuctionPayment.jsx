d96da9fff2d4198d0f52745befa47df97a9a2759
sehyunf <ssam010925@gmail.com>
2025-05-20 11:58:21 +0900

    feat: auctionPayment 페이지 디자인 완료

--- src/pages/auction/auctionPayment/AuctionPayment.jsx
+++ src/pages/auction/auctionPayment/AuctionPayment.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import S from './style';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const AuctionPayment = () => {
+
+  const paymentParam = useParams();
+  const auctionid = paymentParam.id
+
+  console.log(auctionid);
+  
+  
+
   return (
     <S.Wrapper>
       <S.Payment>
@@ -9,80 +18,89 @@ const AuctionPayment = () => {
           <S.auctionImg id="auction-img" src="../assets/images/display/art/sculpture/img-sculpture-cat-2.jpeg" alt="경매 작품" />
         </S.imgWrapper>
         
-        <div>
+        <S.StyledForm>
           <S.H3>낙찰을 축하드립니다!</S.H3>
           {/* <!-- 낙찰 금액 --> */}
-          <div id="success-price">
-            <div id="price">
-              <h5>낙찰 금액<span>|</span></h5>
-              <h5>999,999,999 KRW</h5>
-            </div>
-          </div>
-          
-        <div id="input-form">
+          <S.Price>
+            <S.H5>낙찰 금액 | 999,999,999 KRW</S.H5>
+          </S.Price>
+      
           {/* <!-- 이름 --> */}
-          <div class="input-wrapper">
-            <div class="input-text">
-              <h5>이름<span class="star">*</span></h5>
-              <input id="name" type="text" placeholder="이름을 입력하세요." />
-            </div>
-            <p id="name-error">*필수 항목입니다</p>
-          </div>
+          <S.InputWrapper>
+            <S.InputText>
+              <S.H5>이름<S.RedStar>*</S.RedStar></S.H5>
+              <S.PaymentInput type="text" placeholder="이름을 입력하세요." />
+            </S.InputText>
+          </S.InputWrapper>
+          <S.WarningArea>
+            <S.WarningText>필수 항목입니다</S.WarningText>
+          </S.WarningArea>
           
           {/* <!-- 주소 --> */}
-          <div id="input-button-wrapper">
-            <div class="input-text">
-              <h5>주소<span class="star">*</span></h5>
-              <input id="address" type="text" placeholder="주소를 검색하세요." name="address" required/>
-            </div>
-            <button type="button" id="search-address" onclick="execDaumPostcode()">주소 검색</button>
-          </div>
+          <S.InputWrapper>
+            <S.InputText>
+              <S.H5>주소<S.RedStar>*</S.RedStar></S.H5>
+              <S.PaymentInput type="text" placeholder="주소를 입력하세요." />
+            </S.InputText>
+
+          </S.InputWrapper>
+          <S.WarningArea>
+            <S.WarningText>필수 항목입니다</S.WarningText>
+          </S.WarningArea>
           
           {/* <!-- 상세 주소 --> */}
-          <div class="input-wrapper">
-            <div class="input-text2">
-              <h5>상세 주소<span class="star">*</span></h5>
-              <input id="detail-address" type="text" placeholder="상세 주소를 입력하세요." />
-            </div>
-            <p id="detail-address-error">*필수 항목입니다</p>
-          </div>
+          <S.InputWrapper>
+            <S.InputText2>
+              <S.H5>상세 주소<S.RedStar>*</S.RedStar></S.H5>
+              <S.PaymentInput type="text" placeholder="상세 주소를 입력하세요." />
+            </S.InputText2>
+          </S.InputWrapper>
+          <S.WarningArea>
+            <S.WarningText>필수 항목입니다</S.WarningText>
+          </S.WarningArea>
           
           {/* <!-- 전화번호 --> */}
-          <div class="input-wrapper">
-            <div class="input-text3">
-              <h5>전화번호<span class="star">*</span></h5>
-              <input id="phone" type="text" placeholder="전화번호를 입력하세요." />
-            </div>
-            <p id="phone-error">*필수 항목입니다</p>
-          </div>
-          
+          <S.InputWrapper>
+            <S.InputText3>
+              <S.H5>전화번호<S.RedStar>*</S.RedStar></S.H5>
+              <S.PaymentInput type="text" placeholder="전화번호를 입력하세요." />
+            </S.InputText3>
+          </S.InputWrapper>
+          <S.WarningArea>
+            <S.WarningText>필수 항목입니다</S.WarningText>
+          </S.WarningArea>
+
           {/* <!-- 이메일 --> */}
-          <div class="input-wrapper">
-            <div class="input-text4">
-              <h5>이메일<span class="star">*</span></h5>
-              <input id="email" type="text" placeholder="이메일을 입력하세요." />
-            </div>
-            <p id="email-error">*필수 항목입니다</p>
-          </div>
-        </div>
+          <S.InputWrapper>
+            <S.InputText4>
+              <S.H5>이메일<S.RedStar>*</S.RedStar></S.H5>
+              <S.PaymentInput type="text" placeholder="이메일을 입력하세요." />
+            </S.InputText4>
+          </S.InputWrapper>
+          <S.WarningArea>
+            <S.WarningText>필수 항목입니다</S.WarningText>
+          </S.WarningArea>
+
+          <S.StyledLabel>
+            <S.StyledInput type='checkbox'/>
+            <S.CheckLabelSpan>주문자 본인이 맞습니까?</S.CheckLabelSpan>
+          </S.StyledLabel>
           
-          <input class="check-label" type="checkbox" id="check-user"/>
-              <label class="checkbox-label" for="check-user">
-                <span class="check-label-span">주문자 본인이 맞습니까?</span>
-              </label>
-          <input class="check-label" type="checkbox" id="check-payment"/>
-              <label class="checkbox-label" for="check-payment">
-                <span class="check-label-span">결제에 동의하십니까?</span>
-              </label>
-        </div>
-    
+          <S.CheckboxDiv>
+            <S.StyledLabel>
+              <S.StyledInput type='checkbox'/>
+              <S.CheckLabelSpan>결제에 동의하십니까?</S.CheckLabelSpan>
+            </S.StyledLabel>
+          </S.CheckboxDiv>
+          <S.WarningArea>
+            <S.LastWarningText>필수 동의 항목입니다</S.LastWarningText>
+          </S.WarningArea>
+
+          <S.PaymentButtonWrap>
+            <S.PaymentButton >결제하기</S.PaymentButton>
+          </S.PaymentButtonWrap>
+        </S.StyledForm>
     </S.Payment>
-          <button id="payment-button" onclick="openPopup()">결제하기</button>
-          
-            <div id="overlay"></div>
-          <div id="popup-wrapper">         
-            {/* <%@ include file="./auction-payment-success.jsp"%> */}
-        </div>  
 	</S.Wrapper>
   );
 };
--- src/pages/auction/auctionPayment/style.js
+++ src/pages/auction/auctionPayment/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { H3 } from '../../../styles/common';
+import { EN_H6, H10, H3, H5, H7, H8 } from '../../../styles/common';
 const primary = "#EE3333";
 const gray100 = "#FBFCFC";
 const gray500 = "#C0C5C7";
@@ -11,6 +11,26 @@ const S = {};
 
 S.H3 = styled.p`
   ${H3}
+`
+
+S.H5 = styled.p`
+  ${H5}
+`
+
+S.RedStar = styled.span`
+  ${H5}
+  color: ${primary};
+`
+
+S.WarningText = styled.p`
+  ${H10}
+  color: ${warning};
+  margin: 5px 0 0 108px;
+`
+
+S.LastWarningText = styled.p`
+  ${H10}
+  color: ${warning};
 `
 
 S.Wrapper = styled.div`
@@ -29,20 +49,152 @@ S.Payment = styled.div`
 S.imgWrapper = styled.div`
   display : flex;
 	justify-content : center;
-	align-items : center;
 	width : 560px;
-	height : 560px;
+  margin-bottom: 200px;
 `
 
 S.auctionImg = styled.img`
-  width : 420px;
-	height : 420px;
+  width : 560px;
+  height: 1000px;
 	background-color : #c0c5c7;
 	display : flex;
-	justify-content : center;
-	align-items : center;
+  flex-direction: column;
 `
 
+S.StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 480px;
+`
+
+S.Price = styled.div`
+  display : flex;
+	margin : 39px 0 0 0;
+`
+
+S.InputForm = styled.div`
+  margin : 0 0 20px 0;
+`
+
+S.InputWrapper = styled.div`
+  border-bottom : solid 1px ${gray900};
+	margin : 30px 0 0 0;
+	display : flex;
+`
+
+S.InputText = styled.div`
+  display : flex;
+	gap : 67px;
+	height : 28px;
+`
+
+S.InputText2 = styled.div`
+  display : flex;
+	gap : 32px;
+	height : 28px;
+`
+
+S.InputText3 = styled.div`
+  display : flex;
+	gap : 36px;
+	height : 28px;
+`
+
+S.InputText4 = styled.div`
+  display : flex;
+	gap : 50px;
+	height : 28px;
+`
+
+S.PaymentInput = styled.input`
+  ${EN_H6}
+  color: ${gray900};
+  border : none;
+  outline : none;
+  width : 300px;
+  background-color : ${gray100};
+`
+
+S.WarningArea = styled.div`
+  height: 20px;
+`
+
+S.InputButtonWrapper = styled.div`
+  border-bottom : solid 1px var(--color-gray900);
+	width : 480px;
+	margin : 39px 0 0 0;
+	display : flex;
+	align-items : center;
+	justify-content : space-between;
+`
+
+S.SearchAddress = styled.button`
+  ${H7}
+  width : 125px;
+	height : 35px;
+	background-color : ${primary};
+	border : solid 1px ${primary};
+	border-radius : 2px;
+	color : ${gray100};
+	margin : 0 0 4px 0;
+`
+
+S.CheckLabelSpan = styled.span`
+  ${H8}
+	color : ${gray500};
+`
+
+S.StyledInput = styled.input`
+  margin: 0;
+  appearance: none;
+  border: none;
+  width: 20px;
+  height: 20px;
+  background-image: url('/assets/images/icon/checked_off.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  
+  &:checked {
+    background-image: url('/assets/images/icon/checked_on.png');
+    border-color: transparent;
+    background-size: 100% 100%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+  }
+`
+
+S.CheckboxDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+S.StyledLabel = styled.label`
+  display: flex;
+  cursor: pointer;
+  margin-bottom: 12px;
+`
+
+S.PaymentButtonWrap = styled.div`
+  margin-top: 9px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 200px;;
+`
+
+S.PaymentButton = styled.button`
+  ${H5}
+  width : 190px;
+	height : 50px;
+	background-color : ${gray100};
+	border : solid 1.5px ${primary};
+	border-radius : 3px;
+	color : ${primary};
+  
+  &:hover {
+    background-color : ${primary};
+    color : ${gray100};
+  }
+`
 
 
 
