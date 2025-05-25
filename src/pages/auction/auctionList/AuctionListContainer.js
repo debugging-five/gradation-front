1790abe280e222504adec6f9adfd86fe97a746b6
sehyunf <ssam010925@gmail.com>
2025-05-25 23:56:24 +0900

    fix: auctionList 파싱에러 수정

--- src/pages/auction/auctionList/AuctionListContainer.js
+++ src/pages/auction/auctionList/AuctionListContainer.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import { Link, Navigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import S from './style';
 
 const AuctionListContainer = () => {
-  let { type, category, id } = useParams()
-  id = 36
+  const categoryMap = new Map([
+		["korean", "한국화"],
+		["painting", "회화"],
+		["architecture", "건축"],
+		["sculpture", "조각"],
+		["calligraphy", "서예"],
+		["craft", "공예"]
+  ]);
+  
+  const { category, type } = useParams("category");
+  const [order, setOrder] = useState("");
+  const [cursor, setCursor] = useState(1);
+  const [direction, setDirection] = useState("asc");
+  const [keyword, setKeyword] = useState("");
+  const [auction, setAuction] = useState([]);
 
-  if(!(category === "korean" || category === "sculpture" || category === "craft" || category === "architecture" || category === "calligraphy" || category === "painting" || category === null)) {
-    return <Navigate to="/auction/bidding/korean"></Navigate>
+  const params = {
+    order : order,
+    cursor : cursor,
+    direction : direction,
+    category : categoryMap.get(category) || "한국화",
+    status : type,
+    keyword : keyword,
   }
+
+  useEffect(() => {
+
+    const getAuctionList = async () => {
+      const response = await fetch(`http://localhost:10000/auction/api/list`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(params)
+      })
+      if(!response.ok) {
+        throw new Error(`getDisplayList fetch`)
+      } 
+      const datas = await response.json()
+      console.log(datas);
+      
+
+      return datas;
+    }
+
+    getAuctionList()
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [order, category, cursor, keyword])
   
   
+
   return (
-    <div>
-      리스트 페이지
-      <div>
-        <Link to={`/auction/${type}/${category}/detail/${id}`}>게시글 1번</Link>
-        <Link to={`/auction/${type}/${category}/detail/${id}`}>게시글 2번</Link>
-        <Link to={`/auction/${type}/${category}/detail/${id}`}>게시글 3번</Link>
-      </div>
-    </div>
+    <S.Container>
+    </S.Container>
   );
 };
 
