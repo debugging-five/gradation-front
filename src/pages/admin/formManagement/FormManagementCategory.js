17a27a7ac693a5e29563a2dbe4e7597a4920ae34
duck-goo <djduckgoo@gmail.com>
2025-05-26 17:19:37 +0900

    05/26 17:19

--- src/pages/admin/formManagement/FormManagementCategory.js
+++ src/pages/admin/formManagement/FormManagementCategory.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-const FormManageMentCategory = () => {
+const FormManagementCategory = () => {
   return (
     <div>
       <div>
@@ -15,4 +15,4 @@ const FormManageMentCategory = () => {
   );
 };
 
-export default FormManageMentCategory;
+export default FormManagementCategory;
